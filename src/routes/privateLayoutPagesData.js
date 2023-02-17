835d0f6c036ccc7c02845f907805fd077dfbd7c9
BLeo16 <bleonardo1621@gmail.com>
2023-02-17 13:55:41 -0500

    taxonomia_familia

--- src/routes/privateLayoutPagesData.js
+++ src/routes/privateLayoutPagesData.js
@@ -1,5 +1,6 @@
 import Accesiones from "../pages/Accesiones";
 import TaxonomiaGeneros from "../pages/TaxonomiaGeneros";
+import TaxonomiaFamilias from "../pages/TaxonomiaFamilias";
 const PagesData = [
     {
         path: "/accesiones",
@@ -10,6 +11,11 @@ const PagesData = [
         path: "/taxonomia/generos",
         element: <TaxonomiaGeneros />,
         title: "taxonomia generos"
+    },
+    {
+        path: "/taxonomia/familias",
+        element: <TaxonomiaFamilias />,
+        title: "taxonomia familias"
     }
 ];
 
